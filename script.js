fbc4deb9df004fa40b2b78fcc31b0a4a9b3a192a
manulzvz13 <manulzvz@gmail.com>
2025-06-03 11:09:00 -0600

    feat: Add variables for the calculator operations

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ function divide (numOne, numTwo) {
     return numOne / numTwo;
 }
 
+// Creation of variables for the calculator operation
+let numOne; // 1, 3, 5.3, etc
+let numTwo; // 
+let operator; // + - / *
+
+
+
 
 
 
