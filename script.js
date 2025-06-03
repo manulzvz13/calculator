40a95608829dfeff823ecfda459e382f2e6e5494
manulzvz13 <manulzvz@gmail.com>
2025-06-03 11:31:13 -0600

    fix(operate-function): Ensure operate function returns calculated value

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function add (numOne, numTwo) {
     return numOne + numTwo;
 };
 
-function substract (numOne, numTwo) {
+function subtract (numOne, numTwo) {
     return numOne - numTwo;
 }
 
@@ -12,16 +12,34 @@ function multiply (numOne, numTwo) {
     return numOne * numTwo;
 }
 
-function divide (numOne, numTwo) {
-    return numOne / numTwo;
+function divide(numOne, numTwo) {
+  // Good practice: handle division by zero
+  if (numTwo === 0) {
+    return "Error: Cannot divide by zero!";
+  }
+  return numOne / numTwo;
 }
 
-// Creation of variables for the calculator operation
-let numOne; // 1, 3, 5.3, etc
-let numTwo; // 
-let operator; // + - / *
+// Function to make the different basic math functions with variables above
 
+function operate (symbol, itemOne, itemTwo) {
+    switch (symbol) {
+        case '+':
+            return add(itemOne, itemTwo);
+        case '-':
+            return subtract(itemOne, itemTwo);
+        case '*':
+            return multiply(itemOne, itemTwo);
+        case '/':
+            return divide(itemOne, itemTwo);
+            
+        default:
+            alert('Please input a valid operation (+-*/)');
+            return undefined;
+    }
+};
 
+alert(operate('*',1,4));
 
 
 
